b51fca2607bbe62f4b733ac31b100c9b9b60e5d8
whosnono <111985692+whosnono@users.noreply.github.com>
2022-09-21 18:46:22 -0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+
+package Recursividad;
+
+import java.util.Scanner;
+
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner en = new Scanner(System.in);
+        System.out.println("Teclee el número hasta el que quiere llegar: ");
+        int n = en.nextInt();
+        System.out.println(" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + ", ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+    
+    public static int fibonacci(int n){ //n = numero de iteraciones
+        
+        if(n == 0 || n == 1){
+            return n;
+        } else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
